55942fcd41538e156c7cf568bf62bb5a0b871534
Oviyaa-13 <oviyaa.b2022cse@sece.ac.in>
2024-03-01 15:21:47 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+console.log("Hello")
+/**
+ * git clone <link>
+ * 
+ * git add .
+ * git commit -m "any msg"
+ * git push origin main
+ * 
+ * git config --global user.name "Your Name"
+ * git config --global user.email "Your email"
+ */
